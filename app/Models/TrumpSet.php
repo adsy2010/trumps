135b522278cdac4e4dc6341178717f6506edc2d3
adsy2010 <adsy2010@users.noreply.github.com>
2021-05-10 22:10:56 +0100

    Added return types to cards and stat types relationships

--- app/Models/TrumpSet.php
+++ app/Models/TrumpSet.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class TrumpSet extends Model
 {
@@ -11,13 +13,13 @@ class TrumpSet extends Model
 
     protected $with = ['cards', 'statTypes'];
 
-    public function cards()
+    public function cards(): HasMany
     {
         return $this->hasMany(TrumpCard::class, 'trump_set_id', 'id');
     }
 
 
-    public function statTypes()
+    public function statTypes(): HasManyThrough
     {
         return $this->hasManyThrough(TrumpStatType::class, TrumpSetStatType::class, 'trump_set_id', 'id', 'id', 'trump_stat_type_id');
     }
