f6ab4bf4d3640133a83b3072c4cb1b12c1765cf2
adsy2010 <adsy2010@users.noreply.github.com>
2021-05-10 22:11:37 +0100

    Added player card relationship Added round relationship Added player relationship

--- app/Models/TrumpRoundCard.php
+++ app/Models/TrumpRoundCard.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TrumpRoundCard extends Model
 {
     use HasFactory;
+
+    public function playerCard(): BelongsTo
+    {
+        return $this->belongsTo(TrumpGamePlayerCard::class);
+    }
+
+    public function round(): BelongsTo
+    {
+        return $this->belongsTo(TrumpRound::class);
+    }
+
+    public function player(): BelongsTo
+    {
+        return $this->belongsTo(TrumpPlayer::class);
+    }
 }
